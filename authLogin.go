2242332754f923a9468c3c75995353ea55cb26ad
Oleg Neumyvakin <oneumyvakin@plesk.com>
2019-09-21 15:16:21 +0700

    Add AuthLogin

---
+++ authLogin.go
@@ -0,0 +1,20 @@
+package solus
+
+type AuthLoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthLoginResponseData struct {
+	Data AuthLoginResponse `json:"data"`
+}
+
+type AuthLoginResponse struct {
+	Credentials Credentials `json:"credentials"`
+}
+
+type Credentials struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresAt   string `json:"expires_at"`
+}
---
+++ solus.go
@@ -0,0 +1,112 @@
+package solus
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+type Client struct {
+	BaseURL    *url.URL
+	UserAgent  string
+	Email      string
+	Password   string
+	Token      string
+	Headers    http.Header
+	HttpClient *http.Client
+	Logger     *log.Logger
+}
+
+func NewClient(baseURL *url.URL, email, password string) *Client {
+	return &Client{
+		BaseURL:   baseURL,
+		UserAgent: "solus.io Go client",
+		Email:     email,
+		Password:  password,
+		Headers: map[string][]string{
+			"Accept": {"application/json"},
+		},
+		HttpClient: &http.Client{
+			Timeout: 35,
+		},
+		Logger: log.New(os.Stderr, "", 0),
+	}
+
+}
+
+func (c *Client) request(ctx context.Context, method, path string, body interface{}) ([]byte, int, error) {
+	var reqBody io.ReadWriter
+	if body != nil {
+		reqBody = bytes.NewBuffer([]byte{})
+		if err := json.NewEncoder(reqBody).Encode(body); err != nil {
+			return nil, 0, err
+		}
+	}
+
+	c.Logger.Println(method, c.BaseURL.String()+path, reqBody)
+
+	req, err := http.NewRequestWithContext(ctx, method, c.BaseURL.String()+path, reqBody)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	for k, values := range c.Headers {
+		for _, v := range values {
+			req.Header.Add(k, v)
+		}
+	}
+
+	req.Header.Set("User-Agent", c.UserAgent)
+
+	resp, err := c.HttpClient.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			c.Logger.Println("failed to close body", method, path)
+		}
+	}()
+
+	code := resp.StatusCode
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	if code == 404 {
+		return respBody, code, fmt.Errorf("HTTP %d: %s", code, respBody)
+	}
+
+	if code >= 500 {
+		return respBody, code, fmt.Errorf("HTTP %d: %s", code, respBody)
+	}
+
+	return respBody, code, nil
+}
+
+func (c *Client) AuthLogin(ctx context.Context, data AuthLoginRequest) (AuthLoginResponse, error) {
+	body, code, err := c.request(ctx, "POST", "/auth/login", data)
+	if err != nil {
+		return AuthLoginResponse{}, err
+	}
+
+	if code != 200 {
+		return AuthLoginResponse{}, fmt.Errorf("HTTP %d: %s", code, body)
+	}
+
+	var resp AuthLoginResponseData
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return AuthLoginResponse{}, fmt.Errorf("failed to decode '%s': %s", body, err)
+	}
+
+	return resp.Data, nil
+}
