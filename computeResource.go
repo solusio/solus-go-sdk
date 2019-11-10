8596cb634219da3b2447f7c74499ac236afe30ca
Oleg Neumyvakin <oneumyvakin@plesk.com>
2019-11-10 15:49:59 +0700

    TASK SIO-792 Add compute resource create

--- computeResource.go
+++ computeResource.go
@@ -1,5 +1,11 @@
 package solus
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
 type ComputeResource struct {
 	Id                   int                    `json:"id"`
 	Name                 string                 `json:"name"`
@@ -7,9 +13,46 @@ type ComputeResource struct {
 	Host                 string                 `json:"host"`
 	AgentPort            int                    `json:"agent_port"`
 	Status               ComputerResourceStatus `json:"status"`
+	Zones                []Zone                 `json:"zones"`
 }
 
 type ComputerResourceStatus struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
+}
+
+type ComputerResourceCreateRequest struct {
+	Name  string `json:"name"`
+	Host  string `json:"host"`
+	Login string `json:"login"`
+	// SSH port number
+	Port int `json:"port"`
+	// Auth type 'lpass' or 'lkey'
+	Type     string `json:"type"`
+	Password string `json:"password"`
+	// SSH private key
+	Key       string `json:"key"`
+	AgentPort int    `json:"agent_port"`
+}
+
+type ComputerResourceResponse struct {
+	Data ComputeResource `json:"data"`
+}
+
+func (c *Client) ComputerResourceCreate(ctx context.Context, data ComputerResourceCreateRequest) (ComputeResource, error) {
+	body, code, err := c.request(ctx, "POST", "compute_resources", data)
+	if err != nil {
+		return ComputeResource{}, err
+	}
+
+	if code != 201 {
+		return ComputeResource{}, fmt.Errorf("HTTP %d: %s", code, body)
+	}
+
+	var resp ComputerResourceResponse
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return ComputeResource{}, fmt.Errorf("failed to decode '%s': %s", body, err)
+	}
+
+	return resp.Data, nil
 }
