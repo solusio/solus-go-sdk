6b205cca3cda9312ff7f1b4be21c03ab1772a2b7
mkorsakov <mkorsakov@plesk.com>
2019-11-19 13:42:49 +0700

    FEATURE SIO-827 remove cr prop

--- computeResource.go
+++ computeResource.go
@@ -17,7 +17,6 @@ const (
 type ComputeResource struct {
 	Id                   int                    `json:"id"`
 	Name                 string                 `json:"name"`
-	CanRetryInstallation bool                   `json:"can_retry_installation"`
 	Host                 string                 `json:"host"`
 	AgentPort            int                    `json:"agent_port"`
 	Status               ComputerResourceStatus `json:"status"`
