151bb81f0fa7ad3842f582118e0d1ce486826115
Oleg Neumyvakin <oneumyvakin@plesk.com>
2019-11-29 19:37:18 +0700

    TASK SIO-933 Add compute resource install steps

---
+++ computeResourcesInstallSteps.go
@@ -0,0 +1,38 @@
+package solus
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+type ComputerResourceInstallStepsResponse struct {
+	Data []ComputerResourceInstallSteps `json:"data"`
+}
+
+type ComputerResourceInstallSteps struct {
+	Id                int    `json:"id"`
+	ComputeResourceId int    `json:"compute_resource_id"`
+	Title             string `json:"title"`
+	Status            string `json:"status"`
+	StatusText        string `json:"status_text"`
+	Progress          int    `json:"progress"`
+}
+
+func (c *Client) ComputerResourceInstallSteps(ctx context.Context, id int) ([]ComputerResourceInstallSteps, error) {
+	body, code, err := c.request(ctx, "GET", fmt.Sprintf("compute_resources/%d/install_steps", id), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if code != 200 {
+		return nil, fmt.Errorf("HTTP %d: %s", code, body)
+	}
+
+	var resp ComputerResourceInstallStepsResponse
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return nil, fmt.Errorf("failed to decode '%s': %s", body, err)
+	}
+
+	return resp.Data, nil
+}
