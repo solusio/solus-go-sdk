0599e54cfbd4e2e62dd9ba907e514c719b0ee865
Alexandr Chernev <achernev@plesk.com>
2019-12-31 14:24:57 +0700

    TASK SIO-1078 add user create data

--- users.go
+++ users.go
@@ -1,6 +1,40 @@
 package solus
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
 type User struct {
 	Id    int    `json:"id"`
 	Email string `json:"email"`
+}
+
+type UserCreateRequest struct {
+	Password string `json:"password,omitempty"`
+	Email    string `json:"email,omitempty"`
+	Roles    []int  `json:"roles,omitempty"`
+}
+
+type UserCreateResponse struct {
+	Data User `json:"data"`
+}
+
+func (c *Client) UserCreate(ctx context.Context, data UserCreateRequest) (User, error) {
+	body, code, err := c.request(ctx, "POST", "users", data)
+	if err != nil {
+		return User{}, err
+	}
+
+	if code != 201 {
+		return User{}, fmt.Errorf("HTTP %d: %s", code, body)
+	}
+
+	var resp UserCreateResponse
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return User{}, fmt.Errorf("failed to decode '%s': %s", body, err)
+	}
+
+	return resp.Data, nil
 }
