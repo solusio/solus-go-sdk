0f88d5bb940155fc879765f058630c499fdcffb3
Oleg Neumyvakin <oneumyvakin@plesk.com>
2020-06-10 19:33:15 +0700

    TASK SIO-1935 Add Users, DeleteUser, Servers

--- users.go
+++ users.go
@@ -11,6 +11,10 @@ type User struct {
 	Email string `json:"email"`
 }
 
+type UsersResponse struct {
+	Data []User `json:"data"`
+}
+
 type UserCreateRequest struct {
 	Password string `json:"password,omitempty"`
 	Email    string `json:"email,omitempty"`
@@ -19,6 +23,24 @@ type UserCreateRequest struct {
 
 type UserCreateResponse struct {
 	Data User `json:"data"`
+}
+
+func (c *Client) Users(ctx context.Context) ([]User, error) {
+	body, code, err := c.request(ctx, "GET", "users", nil)
+	if err != nil {
+		return []User{}, err
+	}
+
+	if code != 200 {
+		return []User{}, fmt.Errorf("HTTP %d: %s", code, body)
+	}
+
+	var resp UsersResponse
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return []User{}, fmt.Errorf("failed to decode '%s': %s", body, err)
+	}
+
+	return resp.Data, nil
 }
 
 func (c *Client) UserCreate(ctx context.Context, data UserCreateRequest) (User, error) {
@@ -37,4 +59,17 @@ func (c *Client) UserCreate(ctx context.Context, data UserCreateRequest) (User, 
 	}
 
 	return resp.Data, nil
+}
+
+func (c *Client) UserDelete(ctx context.Context, userId int) error {
+	body, code, err := c.request(ctx, "DELETE", fmt.Sprintf("users/%d", userId), nil)
+	if err != nil {
+		return err
+	}
+
+	if code != 204 {
+		return fmt.Errorf("HTTP %d: %s", code, body)
+	}
+
+	return nil
 }
