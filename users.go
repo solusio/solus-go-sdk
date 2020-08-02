c3ec609630970b1b0e5a4d257b4523645b8dc8ed
Oleg Neumyvakin <oneumyvakin@plesk.com>
2020-08-02 17:44:03 +0700

    BUGFIX SIO-2103 There is impossible to retrieve all users

--- users.go
+++ users.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/url"
 )
 
 const (
@@ -19,10 +20,16 @@ type User struct {
 	Email string `json:"email"`
 	// CreatedAt for date in RFC3339Nano format
 	CreatedAt string `json:"created_at"`
+	Status    string `json:"status"`
 }
 
 type UsersResponse struct {
-	Data []User `json:"data"`
+	Data    []User        `json:"data"`
+	Links   ResponseLinks `json:"links"`
+	Meta    ResponseMeta  `json:"meta"`
+	err     error
+	service *UsersService
+	opts    requestOpts
 }
 
 type UserCreateRequest struct {
@@ -37,24 +44,94 @@ type UserCreateResponse struct {
 	Data User `json:"data"`
 }
 
-func (s *UsersService) List(ctx context.Context, filter *FilterUsers) ([]User, error) {
+func (s *UsersService) List(ctx context.Context, filter *FilterUsers) (UsersResponse, error) {
+	resp := UsersResponse{}
+
 	opts := newRequestOpts()
 	opts.params = filterToParams(filter.Get())
+
+	resp.opts = opts
+	resp.service = s
+
 	body, code, err := s.client.request(ctx, "GET", "users", withParams(opts))
 	if err != nil {
-		return []User{}, err
+		return resp, err
 	}
 
 	if code != 200 {
-		return []User{}, fmt.Errorf("HTTP %d: %s", code, body)
+		return resp, fmt.Errorf("HTTP %d: %s", code, body)
+	}
+
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return resp, fmt.Errorf("failed to decode '%s': %s", body, err)
+	}
+
+	return resp, nil
+}
+
+// Next using for retrieving all data entities
+//
+// Examples:
+//
+//  ctx, cancelFunc := context.WithTimeout(context.Background(), 30 * time.Second)
+//	defer cancelFunc()
+//	usersResponse, err := io.Users.List(ctx, solus.NewFilterUsers())
+//	if err != err {
+//		return err
+//	}
+//
+//	for usersResponse.Next(ctx) {
+//		if err := usersResponse.Err(); err != nil {
+//			return err
+//		}
+//	}
+//
+//  // all your data now in usersResponse.Data
+//	for _, u := range usersResponse.Data {
+//
+//  }
+//
+func (ur *UsersResponse) Next(ctx context.Context) bool {
+	if ur.Links.Next == "" {
+		return false
+	}
+
+	nextUrl, err := url.Parse(ur.Links.Next)
+	if err != nil {
+		ur.err = err
+		return false
+	}
+
+	for k, v := range nextUrl.Query() {
+		ur.opts.params[k] = v
+	}
+
+	body, code, err := ur.service.client.request(ctx, "GET", "users", withParams(ur.opts))
+	if err != nil {
+		ur.err = err
+		return false
+	}
+
+	if code != 200 {
+		ur.err = fmt.Errorf("HTTP %d: %s", code, body)
+		return false
 	}
 
 	var resp UsersResponse
 	if err := json.Unmarshal(body, &resp); err != nil {
-		return []User{}, fmt.Errorf("failed to decode '%s': %s", body, err)
+		ur.err = fmt.Errorf("failed to decode '%s': %s", body, err)
+		return false
 	}
 
-	return resp.Data, nil
+	ur.Data = append(ur.Data, resp.Data...)
+	ur.Meta = resp.Meta
+	ur.Links = resp.Links
+
+	return true
+}
+
+func (ur *UsersResponse) Err() error {
+	return ur.err
 }
 
 func (s *UsersService) Create(ctx context.Context, data UserCreateRequest) (User, error) {
