cc2b216f1045618ac5c4f651af5abeec5e920105
Oleg Neumyvakin <oneumyvakin@plesk.com>
2020-09-26 22:49:59 +0700

    TASK SIO-2396 Add Compute Resource install step status error

--- computeResourcesInstallSteps.go
+++ computeResourcesInstallSteps.go
@@ -6,6 +6,10 @@ import (
 	"fmt"
 )
 
+const (
+	ComputeResourceInstallStepStatusError = "error"
+)
+
 type ComputerResourceInstallStepsResponse struct {
 	Data []ComputerResourceInstallSteps `json:"data"`
 }
