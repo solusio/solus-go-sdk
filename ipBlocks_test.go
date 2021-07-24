423d99b5de1d0a6bb30565d2651a8862804164d2
Dmitry Shemin <shemindmitry@gmail.com>
2021-07-24 13:32:28 +0700

    Enhance ipBlocks

--- ipBlocks_test.go
+++ ipBlocks_test.go
@@ -3,6 +3,7 @@ package solus
 import (
 	"context"
 	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,19 +20,65 @@ func TestIPBlocksService_List(t *testing.T) {
 	s := startTestServer(t, func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "/ip_blocks", r.URL.Path)
 		assert.Equal(t, http.MethodGet, r.Method)
+		assertRequestQuery(t, r, url.Values{
+			"filter[search]": []string{"name"},
+		})
 
 		writeJSON(t, w, http.StatusOK, expected)
 	})
 	defer s.Close()
 
-	actual, err := createTestClient(t, s.URL).IPBlocks.List(context.Background())
+	f := (&FilterIPBlocks{}).ByName("name")
+
+	actual, err := createTestClient(t, s.URL).IPBlocks.List(context.Background(), f)
 	require.NoError(t, err)
 	actual.service = nil
 	require.Equal(t, expected, actual)
 }
 
+func TestIPBlocksService_Get(t *testing.T) {
+	s := startTestServer(t, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/ip_blocks/10", r.URL.Path)
+		assert.Equal(t, http.MethodGet, r.Method)
+
+		writeResponse(t, w, http.StatusOK, fakeIPBlock)
+	})
+	defer s.Close()
+
+	actual, err := createTestClient(t, s.URL).IPBlocks.Get(context.Background(), 10)
+	require.NoError(t, err)
+	require.Equal(t, fakeIPBlock, actual)
+}
+
+func TestIPBlocksService_Update(t *testing.T) {
+	data := IPBlockRequest{
+		ComputeResources: []int{1, 2},
+		Name:             "name",
+		Type:             IPv4,
+		Gateway:          "192.0.2.1",
+		Ns1:              "192.0.2.2",
+		Ns2:              "192.0.2.3",
+		Netmask:          "255.255.255.0",
+		From:             "192.0.2.10",
+		To:               "192.0.2.240",
+	}
+
+	s := startTestServer(t, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/ip_blocks/10", r.URL.Path)
+		assert.Equal(t, http.MethodPut, r.Method)
+		assertRequestBody(t, r, data)
+
+		writeResponse(t, w, http.StatusOK, fakeIPBlock)
+	})
+	defer s.Close()
+
+	actual, err := createTestClient(t, s.URL).IPBlocks.Update(context.Background(), 10, data)
+	require.NoError(t, err)
+	require.Equal(t, fakeIPBlock, actual)
+}
+
 func TestIPBlocksService_Create(t *testing.T) {
-	data := IPBlockCreateRequest{
+	data := IPBlockRequest{
 		ComputeResources: []int{1, 2},
 		Name:             "name",
 		Type:             IPv4,
