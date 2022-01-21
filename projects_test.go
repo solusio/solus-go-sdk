0ac190b35621e9d9f91a4858a0f990db50223074
Dmitry Shemin <shemindmitry@gmail.com>
2022-01-21 11:44:14 +0700

    SIO-4025 Support filtering projects

--- projects_test.go
+++ projects_test.go
@@ -3,6 +3,7 @@ package solus
 import (
 	"context"
 	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -39,12 +40,17 @@ func TestProjectsService_List(t *testing.T) {
 	s := startTestServer(t, func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "/projects", r.URL.Path)
 		assert.Equal(t, http.MethodGet, r.Method)
+		assertRequestQuery(t, r, url.Values{
+			"filter[search]": []string{"name"},
+		})
 
 		writeJSON(t, w, http.StatusOK, expected)
 	})
 	defer s.Close()
 
-	actual, err := createTestClient(t, s.URL).Projects.List(context.Background())
+	f := (&FilterProjects{}).ByName("name")
+
+	actual, err := createTestClient(t, s.URL).Projects.List(context.Background(), f)
 	require.NoError(t, err)
 	actual.service = nil
 	require.Equal(t, expected, actual)
